10247e60766fdb59a9b34c3cd5b97bc7d69ac0f5
Lemon <165233560+BludIsAnLemon@users.noreply.github.com>
2024-10-03 17:43:59 +0800

    Update main.js

--- main.js
+++ main.js
@@ -22,3 +22,20 @@ button.addEventListener('click', function() {
     element.textContent = colour;
   })
 })
+
+const captureZone = document.querySelector(".capture-Zone");
+
+const downloadBtn = document.getElementById('downloadBtn');
+
+downloadBtn.addEventListener('click', function() {
+  domtoimage.toPng(captureZone)
+  .then(function(dataUrl) {
+    const link = document.createElement('a');
+    link.href = dataUrl;
+    link.download = 'Colour Pallete.png';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  })
+  .catch(error => console.error(error)); 
+})
